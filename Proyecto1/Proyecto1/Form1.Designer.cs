4b93bcffb48a2b8c905109fd19c3047ba59e7436
Arán Martínez <aranmarh@gmail.com>
2020-03-17 17:57:28 -0600

    prueba con el manejo en ventana

--- Proyecto1/Proyecto1/Form1.Designer.cs
+++ Proyecto1/Proyecto1/Form1.Designer.cs
@@ -40,8 +40,6 @@
             this.guardarTokensToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.guardarErroresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -65,7 +63,6 @@
             // 
             this.panel1.AutoScroll = true;
             this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.panel1.Controls.Add(this.textBox2);
             this.panel1.Location = new System.Drawing.Point(577, 72);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(428, 195);
@@ -142,13 +139,6 @@
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(104, 106);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 0;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -162,8 +152,6 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -184,7 +172,6 @@
         private System.Windows.Forms.ToolStripMenuItem guardarTokensToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem guardarErroresToolStripMenuItem;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
